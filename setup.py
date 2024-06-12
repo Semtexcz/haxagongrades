972ecaae4380ef5583a133e264b69289c85c3d65
Daniel Kopecký <kopecky.d@gmail.com>
2024-06-12 09:39:41 +0200

    Updated pyproject and setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ package_data = \
 
 install_requires = \
 ['click>=8.1.7,<9.0.0',
+ 'loguru>=0.7.2,<0.8.0',
  'pandas>=2.2.2,<3.0.0',
  'selenium>=4.21.0,<5.0.0',
  'tqdm>=4.66.4,<5.0.0',
@@ -19,9 +20,9 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'haxagongrades',
-    'version': '0.1.1',
+    'version': '1.0.0a1',
     'description': '',
-    'long_description': '# Hexagon Grades',
+    'long_description': '# Hexagon Grades\n\n\n\nHaxagon Grades CLI is a command-line tool designed to manage and generate reports for Haxagon classes. The tool allows you to fetch and export class reports in different formats.\n\n## Features\n\n- Generate class reports in CSV or JSON format.\n- Specify output directory for the generated reports.\n- Supports headless mode for automated environments.\n\n## Installation\n\nInstall the CLI using `pipx` to keep it isolated from your system Python packages:\n\n```bash\npipx install haxagongrades\n```\n\nMake sure you have `pipx` installed. If not, you can install it via pip:\n\n```bash\npip install pipx\npipx ensurepath\n```\n\n## Usage\n\nAfter installing, you can use the `haxagongrades` command to interact with the CLI.\n\n### Commands\n\n#### `report`\n\nGenerate a report for a specific class.\n\n```bash\nhaxagongrades report <class_name> [OPTIONS]\n```\n\n##### Options\n\n- `-f, --format [csv|json]` (Required): Choose the output format.\n- `-o, --output PATH`: The path to the output folder (default is `/tmp`).\n- `-l, --login TEXT`: The username to login with.\n- `-p, --password TEXT`: The password to login with.\n- `--headless`: Run in headless mode (default is `False`).\n\n### Examples\n\nGenerate a report in CSV format for class `3.A` and save it to the `/reports` directory:\n\n```bash\nhaxagongrades report "3.A" --format csv --output /reports\n```\n\nGenerate a JSON report for class `3.A` using headless mode:\n\n```bash\nhaxagongrades report "3.A" --format json --headless\n```\n\n## Development\n\nTo contribute to this project, clone the repository and install the dependencies:\n\n```bash\ngit clone https://github.com/yourusername/haxagon-grades-cli.git\ncd haxagon-grades-cli\npip install -e .\n```\n\n## License\n\nThis project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.\n\n\n',
     'author': 'Daniel Kopecký',
     'author_email': 'kopecky.d@gmail.com',
     'maintainer': 'None',
